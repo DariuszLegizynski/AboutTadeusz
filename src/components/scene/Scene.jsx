9ca233397a61c9752e83f9830ee8fa765cb64a58
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-02-22 12:03:12 +0100

    add basic controls

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -1,18 +1,26 @@
 import { Suspense } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
-// import MapModel from '../mapModel/mapModel'
+import { OrbitControls } from '@react-three/drei'
 
 import './Scene.css'
 
 const Scene = () => {
   const gltf = useLoader(GLTFLoader, 'src/assets/north_america_3d_map/scene.gltf')
+
+  // const deg2rad = degrees => degrees * (Math.PI / 180);
+
+  // useThree(({camera}) => {
+  //   camera.rotation.set(deg2rad(30), 0, 0)
+  // });
+
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 10, position: [0, 0, 5] }}>
+      <hemisphereLight color="white" position={[0, 0, 5]} />
       <Suspense fallback={null}>
-        <primitive object={gltf.scene} />
+        <primitive scale={[1, 1, 1]} object={gltf.scene} />
       </Suspense>
+      <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
     </Canvas>
   )
 }
