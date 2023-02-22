eb26b9c25308da1742e80309b83ea14dd6d4e9d8
Dariusz Legizynski <dariuszlegizynski@lemon42.com>
2023-02-22 12:35:16 +0100

    add screen on map

--- src/components/scene/Scene.jsx
+++ src/components/scene/Scene.jsx
@@ -1,24 +1,21 @@
 import { Suspense } from 'react'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OrbitControls } from '@react-three/drei'
+import { Bounds, useBounds, OrbitControls, ContactShadows, useGLTF } from '@react-three/drei'
 
 import './Scene.css'
 
 const Scene = () => {
   const gltf = useLoader(GLTFLoader, 'src/assets/north_america_3d_map/scene.gltf')
-
-  // const deg2rad = degrees => degrees * (Math.PI / 180);
-
-  // useThree(({camera}) => {
-  //   camera.rotation.set(deg2rad(30), 0, 0)
-  // });
+  const tv = useLoader(GLTFLoader,'src/assets/tv_screen/scene.gltf')
+  // return <primitve scale={[1, 1, 1]} object={tv.scene} />
 
   return (
-    <Canvas camera={{ fov: 10, position: [0, 0, 5] }}>
-      <hemisphereLight color="white" position={[0, 0, 5]} />
+    <Canvas camera={{ fov: 10, position: [0, 0, 10] }}>
+      <hemisphereLight color="white" position={[0, 0, 10]} />
       <Suspense fallback={null}>
         <primitive scale={[1, 1, 1]} object={gltf.scene} />
+        <primitive scale={[0.005, 0.005, 0.005]} position={[0, 0, 2]} rotation={[0.75, 0, 0]} object={tv.scene} />
       </Suspense>
       <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 1.75} />
     </Canvas>
